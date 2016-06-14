ecb833ba5fa657b8f92313db86135102a6897b55
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2016-06-14 11:27:10 +0200

    Expand username url regex

--- ideax/user/urls.py
+++ ideax/user/urls.py
@@ -3,5 +3,5 @@ from ideax.user.views import ProfileView
 
 
 urlpatterns = [
-    url(r'(?P<username>[a-zA-Z0-9]+)$', ProfileView.as_view()),
+    url(r'(?P<username>[-+_@.a-zA-Z0-9]+)$', ProfileView.as_view()),
 ]
